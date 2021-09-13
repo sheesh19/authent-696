06fda4031bbd80918674b4d0bf619fafdf7ba8b2
Sheila Leveille <sbleveille@gmail.com>
2021-09-13 10:06:30 +0300

    ARCH: Initial set up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
+    # current_user is available via devise
+  end
+  
+  def about
   end
 end
